ddcd0cef6ec2057599fa72b5228e878fa42e3f09
Arash Soltani <soltaniworld@gmail.com>
2023-03-14 13:53:15 -0400

    added ability to delete based on title using button on posts

--- app.js
+++ app.js
@@ -11,14 +11,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express();
 
-//all posts
-const posts = [
-  { title: 'Title One', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo modi libero unde aspernatur ex sint expedita earum exercitationem nesciunt, animi in cumque incidunt cupiditate consequuntur labore culpa? Accusamus cupiditate animi aliquid sequi voluptatibus, vel perspiciatis possimus sint vero tempore quod excepturi minima culpa eligendi dolor!' },
-  { title: 'Title Two', body: 'Facilis delectus architecto consectetur tempore debitis commodi ducimus fuga saepe rem voluptas inventore neque quaerat voluptatem consequatur amet ut, nostrum, corrupti beatae. Natus suscipit nisi reprehenderit cupiditate omnis expedita aliquam debitis recusandae alias? Placeat vero itaque odit eveniet ad, dolorem aliquid animi! Modi, qui saepe?' },
-  { title: 'Title Three', body: 'Magni, officiis, rerum natus, autem aspernatur nulla quo voluptatem labore unde sunt nam temporibus? Aliquid vel voluptatibus quia ut nulla! Reprehenderit possimus excepturi quos porro dolore nulla. Molestiae facilis atque beatae quae incidunt necessitatibus impedit, possimus hic ad quia aut quidem esse excepturi nisi cumque?' }
-];
-
-//replacing posts as we migrate to db
+//replacing posts = [] as we migrate to db
 const myPosts = [];
 
 const database = 'Blog';
@@ -55,24 +48,18 @@ app.use(express.static("public"));
 
 //home page route
 app.get('/', (req, res) => {
-  // Retrieve all documents from the "posts" collection and add them to a list named "myPosts"
-  Post.find({}, { _id: 0, title: 1, body: 1 })
-    .then(posts => {
-      myPosts.push.apply(myPosts, posts);
-      console.log(myPosts[4]);
-    })
-    .then(()=>{
-      res.render('home', {
-        homeStartingContent: homeStartingContent,
-        posts: myPosts
-      });
-
-    })
-    .catch(err => {
-      console.error(err);
+  console.log('home page loading');
+  getPosts()
+  .then(()=>{
+    res.render('home', {
+      homeStartingContent: homeStartingContent,
+      posts: myPosts
     });
-
-});
+  })
+  .then(()=>{
+    console.log('home page COMPLETE');
+  });
+})
 
 //about page
 app.get('/about', (req, res) => {
@@ -92,11 +79,42 @@ app.get('/compose', (req, res) => {
 //get route for post page
 app.get('/posts/:post', (req, res) => {
   //find and return the post where title matches :post parameter
-  const post = myPosts.find(post => _.lowerCase(post.title) === _.lowerCase(req.params.post));
-  res.render('post', { post: post });
+  console.log("Posts page: " +  req.params.post);
+  Post.findOne({ title: { $regex: new RegExp(req.params.post, "i") } })
+    .then(post => {
+      if (post) {
+        res.render('post', { post: post });
+      } else {
+        res.status(404).send('Post not found');
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    });
+
 });
 
+// delete post by title
+app.post('/delete/:title', (req, res) => {
+  console.log("delete initiated: " + req.params.title);
+  Post.findOneAndDelete({ title: { $regex: new RegExp(req.params.title, "i") } })
+    .then(post => {
+      if (post) {
+        res.status(204).redirect('/'); // No Content
+      } else {
+        res.status(404).send('Post not found');
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    });
+    // res.redirect('/');
 
+    
+
+});
 
 
 //compose POST route
@@ -106,23 +124,25 @@ app.post('/compose', (req, res) => {
     title: req.body.postTitle,
     body: req.body.postBody
   };
-
-  posts.push(post)
+  myPosts.push(post)
 
   // Create a new blog post object
-  const postOnline = new Post({
-    title: req.body.postTitle,
-    body: req.body.postBody,
-  });
-
+  const postOnline = new Post(post);
   postOnline.save();
 
   res.redirect('/');
-  
+
 });
 
+async function getPosts() {
+  await Post.find({}, { _id: 1, title: 1, body: 1 })
+    .then(posts => {
+      myPosts.push.apply(myPosts, posts);
+    });
+};
 
-
+//pull all new posts and set to array myPosts
+getPosts();
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
