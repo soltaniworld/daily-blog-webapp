97a39d0fa1ed6c4500a84f3bd8690587f177cc38
Arash Soltani <soltaniworld@gmail.com>
2023-03-14 09:57:29 -0400

    added mongoose, and mongodb creds to connect. Can compose blog which  adds entry to database too

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 //const ejs = require("ejs"); // you dont need to require EJS, as long as it is set as viewengine and is installed on  npm.
 const _ = require('lodash'); //use lodash to work with strings, arrays, etc..
-
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,16 +18,45 @@ const posts = [
   { title: 'Title Three', body: 'Magni, officiis, rerum natus, autem aspernatur nulla quo voluptatem labore unde sunt nam temporibus? Aliquid vel voluptatibus quia ut nulla! Reprehenderit possimus excepturi quos porro dolore nulla. Molestiae facilis atque beatae quae incidunt necessitatibus impedit, possimus hic ad quia aut quidem esse excepturi nisi cumque?' }
 ];
 
-app.set('view engine', 'ejs');
+const database = 'Blog';
+const username = 'ArashMongoDB';
+const pw = 'cuxg7fHOBwE62SAY';
+const url = 'firstcluster0.dhjrbp6.mongodb.net';
+// Connect to your remote MongoDB database using Mongoose
+mongoose.connect(`mongodb+srv://${username}:${pw}@${url}/${database}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
 
-app.use(bodyParser.urlencoded({extended: true}));
+//define collection's schema
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+});
+
+//initialize model for each document  based on schema
+//keep in mine the coolection is called 'posts' and I only named the document  as 'post'
+//the document auto added the plural and added to collection 'posts'
+const Post = mongoose.model('post', postSchema);
+
+
+//set view engine to read ejs files
+app.set('view engine', 'ejs');
+//set body parser to read request body
+app.use(bodyParser.urlencoded({ extended: true }));
+//set public folder to be referencable to public folder in the root of the project//set static folder to be referencable to public folder in the root of the project//set public folder to
 app.use(express.static("public"));
 
-//home page
+//home page route
 app.get('/', (req, res) => {
-  res.render('home', { 
+  res.render('home', {
     homeStartingContent: homeStartingContent,
-    posts: posts });
+    posts: posts
+  });
+
+
+  article = Blog.find().exec();
+  console.log(article);
 });
 
 //about page
@@ -58,13 +87,23 @@ app.get('/posts/:post', (req, res) => {
 //compose POST route
 app.post('/compose', (req, res) => {
   //variable to store posts
-  const post ={
+  const post = {
     title: req.body.postTitle,
     body: req.body.postBody
   };
- 
-  posts.push(post)    
+
+  posts.push(post)
+
+  // Create a new blog post object
+  const postOnline = new Post({
+    title: req.body.postTitle,
+    body: req.body.postBody,
+  });
+
+  postOnline.save();
+
   res.redirect('/');
+  
 });
 
 //posts view
@@ -78,6 +117,6 @@ app.get('/posts', (req, res) => {
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
